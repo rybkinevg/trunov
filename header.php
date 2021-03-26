d32923be441363dfbc7095d2ad85ce86554211fa
rybkinevg <boomuk-mc@yandex.ru>
2021-03-26 20:39:48 +0300

    gittest

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Международная Юридическая фирма «Трунов, Айвар и партнеры»</title>
+    <title>Международная Юридическая фирма «Трунов, Айвар и партнёры»</title>
 
     <?php wp_head(); ?>
 </head>
